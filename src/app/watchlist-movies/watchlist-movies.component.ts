4b741535bf9778245b13c793a9da62c2b0df5855
Grzegorz Gil <ggil@sii.pl>
2024-07-19 15:56:49 +0200

    add loader

--- src/app/watchlist-movies/watchlist-movies.component.ts
+++ src/app/watchlist-movies/watchlist-movies.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../service/account/account.service';
-import { catchError, EMPTY, Observable, switchMap, throwError } from 'rxjs';
+import { catchError, EMPTY, finalize, Observable, switchMap, tap } from 'rxjs';
 import { Result } from '../models/result';
 
 @Component({
@@ -9,9 +9,10 @@ import { Result } from '../models/result';
   styleUrls: ['./watchlist-movies.component.scss'],
 })
 export class WatchlistMoviesComponent implements OnInit {
-
   movies: Observable<Result> = EMPTY;
   error?: string;
+  isLoading: boolean = false;
+  hasError: boolean = false;
 
   constructor(private accountService: AccountService) {}
 
@@ -20,13 +21,28 @@ export class WatchlistMoviesComponent implements OnInit {
   }
 
   fetchWatchlist(): Observable<Result> {
-    return this.accountService.getWatchlistMovies().pipe(catchError( err => {this.error = err; return throwError(() => new Error(err))}));
+    this.isLoading = true;
+    this.hasError = false;
+    this.error = undefined;
+    return this.accountService.getWatchlistMovies().pipe(
+      tap(() => {this.hasError = false}),
+      catchError((err) => {
+        this.isLoading = false;
+        this.error = err;
+        this.hasError = true;
+        return EMPTY;
+      }),
+      finalize(() => {this.isLoading = false})
+    );
   }
 
   updateWatchlist(movieId: number) {
-    this.accountService
+    this.movies = this.accountService
       .updateWatchlist(movieId, false)
-      .pipe(switchMap((data) => this.movies =this.accountService.getWatchlistMovies()))
-      .subscribe();
+      .pipe(switchMap(() => this.accountService.getWatchlistMovies()));
+  }
+
+  refreshPage() {
+    this.movies = this.fetchWatchlist();
   }
 }
